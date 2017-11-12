b11fd6629cacbff5316f5d60a2fffc98b962b43f
John Daugherty <johndaugherty1986@gmail.com>
2017-11-11 22:54:17 -0600

    added api call to index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,13 @@
 const API_KEY = 'f6a6fec7e553937429cc047053a9c580';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   }
 }
