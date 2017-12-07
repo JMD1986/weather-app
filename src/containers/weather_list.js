05f848ea07ee9ce6f87b2f7c5d8ca3142cb0cd00
John Daugherty <johndaugherty1986@gmail.com>
2017-12-06 21:31:06 -0600

    mapping state and adding to table

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+
+class WeatherList extends Component {
   render(){
     return (
         <table className="table table-hover">
           <thead>
             <tr>
               <th>city</th>
+              <th>temp</th>
+              <th>pressure</th>
+              <th>Humidy</th>
             </tr>
           </thead>
         </table>
       );
   }
 }
+
+
+function mapStateToProps({weather}){
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
